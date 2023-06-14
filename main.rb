3d5cbdcb37e365c9dea72153e9ed957d1fa056f6
MarwanDev <marwaneg@aucegypt.edu>
2023-06-14 23:27:25 +0300

    Add decode char and word

---
+++ main.rb
@@ -0,0 +1,45 @@
+@dictionary = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(char)
+  current_Char = @dictionary[char]
+  current_Char
+end
+
+def decode_word(word)
+  chars = word.split
+  current_Word = ''
+  chars.each do |char|
+    decode_char(char)
+    current_Word += decode_char(char)
+  end
+  print "#{current_Word} "
+end
+
+
